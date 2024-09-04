08c67c12fa97e4065f43129157d78845eed8a92c
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-04 16:38:58 +0200

    feat: 3x3 led cube

--- 3DCube9Leds/src/main.cpp
+++ 3DCube9Leds/src/main.cpp
@@ -5,18 +5,51 @@ const uint8_t numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
 const uint8_t groundPins[] = {10, 11, 12};
 const uint8_t numGrounds = sizeof(groundPins) / sizeof(groundPins[0]);
 
+void SelfTest();
+
 void setup()
 {
+  Serial.begin(9600);
+  while (!Serial)
+  {
+  }
+
   for (uint8_t i = 0; i < numLeds; i++)
-    pinMode(i, OUTPUT);
+    pinMode(ledPins[i], 0x1);
 
   for (uint8_t i = 0; i < numGrounds; i++)
-  {
-    pinMode(i, OUTPUT);
-    analogWrite(i, 0x1);
-  }
+    pinMode(groundPins[i], 0x1);
+
+  SelfTest();
 }
 
 void loop()
 {
+    for (uint8_t i = 0; i < numLeds; i++)
+      digitalWrite(ledPins[i], 0x1);
+}
+
+void SelfTest()
+{
+  Serial.println("Starting self test.");
+  for (uint8_t i = 0; i < numGrounds; i++)
+  {    
+    digitalWrite(groundPins[i], 0x1);
+    delay(100);
+
+    for (uint8_t i = 0; i < numLeds; i++)
+    {
+      digitalWrite(ledPins[i], 0x1);
+      delay(100);
+    }
+
+    for (uint8_t i = 0; i < numLeds; i++)
+    {
+      digitalWrite(ledPins[i], 0x0);
+      delay(100);
+    }
+
+    digitalWrite(groundPins[i], 0x0);
+    delay(100);
+  }
 }
