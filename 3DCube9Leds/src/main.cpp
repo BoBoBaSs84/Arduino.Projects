79ad01843d0f352d6d611ed22aa61c3116f718c3
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-04 18:25:47 +0200

    feat: led cube
    closes #3

--- 3DCube9Leds/src/main.cpp
+++ 3DCube9Leds/src/main.cpp
@@ -2,11 +2,11 @@
 
 const uint8_t groundPins[] = {14, 15, 16};
 const uint8_t numGrounds = sizeof(groundPins) / sizeof(groundPins[0]);
-const uint8_t ledPins[] = {2, 3, 4, 7, 6, 5, 10, 9, 8};
+const uint8_t ledPins[] = {6, 2, 3, 4, 5, 8, 9, 10, 7};
 const uint8_t numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
 
 void SelfTest(int delayTime);
-void Rotate();
+void Rotate(int delayTime);
 
 void setup()
 {
@@ -31,30 +31,49 @@ void setup()
 
 void loop()
 {
-  SelfTest(5);
+  Rotate(5);
 }
 
 void SelfTest(int delayTime)
 {
-
   Serial.println("Starting self test...");
   for (uint8_t i = 0; i < numGrounds; i++)
   {
     digitalWrite(groundPins[i], 0x0);
-    delay(delayTime);
 
     for (uint8_t i = 0; i < numLeds; i++)
     {
       digitalWrite(ledPins[i], 0x1);
       delay(delayTime);
       digitalWrite(ledPins[i], 0x0);
-      delay(delayTime);
     }
 
     digitalWrite(groundPins[i], 0x1);
   }
+  Serial.println("Finished self test...");
+  delay(delayTime);
 }
 
-void Rotate()
+void Rotate(int delayTime)
 {
+  for (uint8_t l = 1; l <= numLeds; l++)
+  {
+    for (uint8_t i = 0; i < numGrounds; i++)
+    {
+      digitalWrite(groundPins[i], 0x0);
+      digitalWrite(ledPins[0], 0x1);
+      delay(delayTime);
+      digitalWrite(ledPins[0], 0x0);
+      digitalWrite(groundPins[i], 0x1);
+    }
+
+    for (uint8_t g = 0; g < numGrounds; g++)
+    {
+      digitalWrite(groundPins[g], 0x0);
+      digitalWrite(ledPins[l], 0x1);
+      delay(delayTime);
+      digitalWrite(ledPins[l], 0x0);
+      digitalWrite(groundPins[g], 0x1);
+    }
+  }
 }
