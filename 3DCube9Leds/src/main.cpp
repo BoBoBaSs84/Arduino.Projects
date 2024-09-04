b5d502965992ccfb7917ca66e5d8d57225503f12
Robert Peter Meyer <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-04 17:18:08 +0200

    merge: from main (#4)
    * feat: led cubre started
    
    * feat: 3x3 led cube
    
    * feat: getting somewhere

---
+++ 3DCube9Leds/src/main.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+
+const uint8_t groundPins[] = {14, 15, 16};
+const uint8_t numGrounds = sizeof(groundPins) / sizeof(groundPins[0]);
+const uint8_t ledPins[] = {2, 3, 4, 7, 6, 5, 10, 9, 8};
+const uint8_t numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
+
+void SelfTest();
+void Rotate();
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+  {
+  }
+
+  Serial.println("Starting setup...");
+
+  for (uint8_t i = 0; i < numGrounds; i++)
+  {
+    pinMode(groundPins[i], 0x1);
+    digitalWrite(groundPins[i], 0x1);
+  }
+
+  for (uint8_t i = 0; i < numLeds; i++)
+    pinMode(ledPins[i], 0x1);
+
+  SelfTest();
+}
+
+void loop()
+{
+}
+
+void SelfTest()
+{
+  uint8_t delayTime = 100;
+  Serial.println("Starting self test...");
+  for (uint8_t i = 0; i < numGrounds; i++)
+  {
+    digitalWrite(groundPins[i], 0x0);
+    delay(delayTime);
+
+    for (uint8_t i = 0; i < numLeds; i++)
+    {
+      digitalWrite(ledPins[i], 0x1);
+      delay(delayTime);
+      digitalWrite(ledPins[i], 0x0);
+      delay(delayTime);
+    }
+
+    digitalWrite(groundPins[i], 0x1);
+    delay(delayTime);
+  }
+}
+
+void Rotate()
+{
+
+}
