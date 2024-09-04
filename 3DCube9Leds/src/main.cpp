e467c97d31b1cd973bebc71221e0254719a09a41
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-04 17:15:48 +0200

    feat: getting somewhere

--- 3DCube9Leds/src/main.cpp
+++ 3DCube9Leds/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 
-const uint8_t ledPins[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-const uint8_t numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
-const uint8_t groundPins[] = {10, 11, 12};
+const uint8_t groundPins[] = {14, 15, 16};
 const uint8_t numGrounds = sizeof(groundPins) / sizeof(groundPins[0]);
+const uint8_t ledPins[] = {2, 3, 4, 7, 6, 5, 10, 9, 8};
+const uint8_t numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
 
 void SelfTest();
+void Rotate();
 
 void setup()
 {
@@ -14,42 +15,47 @@ void setup()
   {
   }
 
-  for (uint8_t i = 0; i < numLeds; i++)
-    pinMode(ledPins[i], 0x1);
+  Serial.println("Starting setup...");
 
   for (uint8_t i = 0; i < numGrounds; i++)
+  {
     pinMode(groundPins[i], 0x1);
+    digitalWrite(groundPins[i], 0x1);
+  }
+
+  for (uint8_t i = 0; i < numLeds; i++)
+    pinMode(ledPins[i], 0x1);
 
   SelfTest();
 }
 
 void loop()
 {
-    for (uint8_t i = 0; i < numLeds; i++)
-      digitalWrite(ledPins[i], 0x1);
 }
 
 void SelfTest()
 {
-  Serial.println("Starting self test.");
+  uint8_t delayTime = 100;
+  Serial.println("Starting self test...");
   for (uint8_t i = 0; i < numGrounds; i++)
-  {    
-    digitalWrite(groundPins[i], 0x1);
-    delay(100);
+  {
+    digitalWrite(groundPins[i], 0x0);
+    delay(delayTime);
 
     for (uint8_t i = 0; i < numLeds; i++)
     {
       digitalWrite(ledPins[i], 0x1);
-      delay(100);
-    }
-
-    for (uint8_t i = 0; i < numLeds; i++)
-    {
+      delay(delayTime);
       digitalWrite(ledPins[i], 0x0);
-      delay(100);
+      delay(delayTime);
     }
 
-    digitalWrite(groundPins[i], 0x0);
-    delay(100);
+    digitalWrite(groundPins[i], 0x1);
+    delay(delayTime);
   }
+}
+
+void Rotate()
+{
+
 }
