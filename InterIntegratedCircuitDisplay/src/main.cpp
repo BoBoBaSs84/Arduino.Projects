98a220119dfc5a3575995fefb4bcfe5904ec7db1
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-03-11 16:40:50 +0100

    feat: modified the output a bit

--- InterIntegratedCircuitDisplay/src/main.cpp
+++ InterIntegratedCircuitDisplay/src/main.cpp
@@ -6,6 +6,12 @@
 #define LCD_COLS 16 // Number of columns of the LCD
 #define LCD_ROWS 2 // Number of rows of the LCD
 
+long randNumber; // To store a random number
+float randFloat; // To store a random float
+
+void writeFirstLine(LiquidCrystal_I2C lcd, String message);
+void writeSecondLine(LiquidCrystal_I2C lcd, String message);
+
 // Create an LCD object
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(LCD_ADDR, LCD_COLS, LCD_ROWS);
 
@@ -16,15 +22,36 @@ void setup()
   lcd.init();
   // Turn on the backlight
   lcd.backlight();
-}
+  // seed the random number generator
+  randomSeed(analogRead(0));
 
-// the loop routine runs over and over again forever
-void loop()
-{
   // Print a message to the LCD
   lcd.setCursor(0, 0);
   lcd.print("Hello, World!");
   delay(1000);
   lcd.clear();
+}
+
+// the loop routine runs over and over again forever
+void loop()
+{
+  // Generate a random number and a random float
+  randNumber = random(0, 100);
+  randFloat = (float)randNumber / 100.0;
+
+  writeFirstLine(lcd, "Number: " + String(randNumber));
+  writeSecondLine(lcd, "Float: " + String(randFloat, 2));
   delay(1000);
+}
+
+void writeFirstLine(LiquidCrystal_I2C lcd, String message)
+{
+  lcd.setCursor(0, 0);
+  lcd.print(message);
+}
+
+void writeSecondLine(LiquidCrystal_I2C lcd, String message)
+{
+  lcd.setCursor(0, 1);
+  lcd.print(message);
 }
